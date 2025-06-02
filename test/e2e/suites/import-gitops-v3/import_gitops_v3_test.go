9adee3a66e3e24193285a7493fe62cfc41d9fcf0
Michal Jura <mjura@suse.com>
2025-06-02 14:25:56 +0200

    fix3

--- test/e2e/suites/import-gitops-v3/import_gitops_v3_test.go
+++ test/e2e/suites/import-gitops-v3/import_gitops_v3_test.go
@@ -98,7 +98,7 @@ var _ = Describe("[Docker] [Kubeadm]  Create and delete CAPI cluster functionali
 	specs.CreateMgmtV3UsingGitOpsSpec(ctx, func() specs.CreateMgmtV3UsingGitOpsSpecInput {
 		testenv.CAPIOperatorDeployProvider(ctx, testenv.CAPIOperatorDeployProviderInput{
 			BootstrapClusterProxy: bootstrapClusterProxy,
-			CAPIProvidersOCIYAML: []testenv.OCIProvider{
+			CAPIProvidersOCIYAML: []testenv.CAPIProvidersOCIYAML{
 				Name: "docker",
 				File: e2e.CapiProvidersOci,
 			},
